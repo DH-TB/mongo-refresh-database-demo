66d804e458f84a871e00cfe86e3356960e23b488
DH-TB <929659475@qq.com>
2017-04-12 20:48:12 +0800

    add connect database success or failure and readme.md

---
+++ README.md
@@ -0,0 +1,6 @@
+```
+cnpm install //安装了mongoose
+node app.js //查看数据库是否链接
+node route/*.js //查看具体操作
+
+```
--- app.js
+++ app.js
@@ -1,6 +1,18 @@
+// 首先引入 mongoose 这个模块
 let mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/test');
 
+// 端口号省略则默认连接 27017；test 是数据库的名称
+let db = mongoose.connect('mongodb://localhost/test');
+
+db.connection.on("open", function () {
+    console.log("数据库连接成功");
+});
+
+db.connection.on("error", function (error) {
+    console.log("数据库连接失败：" + error);
+});
+
+//默认集合名字为cats
 let Cat = mongoose.model('Cat', {
     name: String,
     friends: [String],
